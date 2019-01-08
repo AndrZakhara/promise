ef83cb0664147108ef4ecdf0d6be8406a0ae36d1
Andrii Zakharchenko <art.pruttov@gmail.com>
2019-01-08 12:09:49 +0200

    feature/constructor, add constructor

--- src/promise.js
+++ src/promise.js
@@ -1,4 +1,14 @@
 class OwnPromise {
+  constructor(executor) {
+    if (typeof executor !== 'function') {
+      throw new TypeError('Executor is not a function');
+    }
+
+    this.state = 'PENDING';
+    this.value = null;
+    this.queue = [];
+    executor(OwnPromise.resolve, OwnPromise.reject);
+  }
 }
 
 module.exports = OwnPromise;
