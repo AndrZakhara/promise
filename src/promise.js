8203cc75255d9a05e763675af2708b94991f3b41
Andrii Zakharchenko <art.pruttov@gmail.com>
2019-01-17 08:37:41 +0200

    fix, then

--- src/promise.js
+++ src/promise.js
@@ -1,34 +1,175 @@
-const RESOLVED = 'RESOLVED';
 const PENDING = 'PENDING';
+const RESOLVED = 'RESOLVED';
 const REJECTED = 'REJECTED';
 
 class OwnPromise {
-  constructor(executor) {
-    if (typeof executor !== 'function') {
-      throw new TypeError('Executor is not a function');
+  constructor(executer) {
+    if (typeof executer !== 'function') {
+      throw new TypeError('Promise resolver must be a function');
     }
+
     this.state = PENDING;
-    this.callbacks = [];
     this.value = null;
+    this.callbacks = [];
+    this.resolve = this.resolve.bind(this);
+    this.reject = this.reject.bind(this);
+    executer(this.resolve, this.reject);
+  }
 
-    const resolve = data => {
-      if (this.state !== PENDING) {
-        return;
-      }
+  resolve(value) {
+    if (this.state === PENDING) {
       this.state = RESOLVED;
-      this.value = data;
-      this.callbacks.forEach(({ res, rej }) => {
-        this.value = res(this.value);
-      });
-    };
+      this.value = value;
 
-    const reject = err => {
+      while (this.callbacks.length > 0) {
+        const cb = this.callbacks.pop();
+
+        if (this.state === RESOLVED) {
+          if (cb.onResolved) {
+            setTimeout(() => cb.onResolved(this.value), 0);
+          }
+        } else if (this.state === REJECTED) {
+          if (cb.onRejected) {
+            setTimeout(() => cb.onRejected(this.value), 0);
+          }
+        } else {
+          this.callbacks.push(cb);
+        }
+      }
+    }
+  }
+
+  reject(value) {
+    if (this.state === PENDING) {
       this.state = REJECTED;
-      this.value = err;
-      return new OwnPromise((resolve, reject) => reject(err));
-    };
+      this.value = value;
 
-    executor(resolve, reject);
+      while (this.callbacks.length > 0) {
+        const cb = this.callbacks.pop();
+
+        if (this.state === RESOLVED) {
+          if (cb.onResolved) {
+            setTimeout(() => cb.onResolved(this.value), 0);
+          }
+        } else if (this.state === REJECTED) {
+          if (cb.onRejected) {
+            setTimeout(() => cb.onRejected(this.value), 0);
+          }
+        } else {
+          this.callbacks.push(cb);
+        }
+      }
+    }
+  }
+
+  then(onResolved, onRejected) {
+    return new OwnPromise((resolve, reject) => {
+      const cb = {
+        onResolved: value => {
+          let nextValue = value;
+
+          if (onResolved) {
+            try {
+              nextValue = onResolved(value);
+
+              if (nextValue && nextValue.then) {
+                return nextValue.then(resolve, reject);
+              }
+            } catch (err) {
+              return reject(err);
+            }
+          }
+          resolve(nextValue);
+        },
+        onRejected: value => {
+          let nextValue = value;
+
+          if (onRejected) {
+            try {
+              nextValue = onRejected(value);
+
+              if (nextValue && nextValue.then) {
+                return nextValue.then(resolve, reject);
+              }
+            } catch (err) {
+              return reject(err);
+            }
+          }
+          reject(nextValue);
+        }
+      };
+
+      if (this.state === RESOLVED) {
+        if (cb.onResolved) {
+          setTimeout(() => cb.onResolved(this.value), 0);
+        }
+      } else if (this.state === REJECTED) {
+        if (cb.onRejected) {
+          setTimeout(() => cb.onRejected(this.value), 0);
+        }
+      } else {
+        this.callbacks.push(cb);
+      }
+    });
+  }
+
+  done(onResolved) {
+    return this.then(onResolved);
+  }
+
+  catch(onRejected) {
+    return this.then(undefined, onRejected);
+  }
+
+  static resolve(value) {
+    if (typeof this !== 'function') {
+      throw new TypeError('this is not a constructor');
+    } else if (value instanceof OwnPromise) {
+      return value;
+    }
+
+    return new this((resolve, reject) => {
+      if (typeof resolve !== 'function' || typeof reject !== 'function') {
+        throw new TypeError('Not a function');
+      }
+      resolve(value);
+    });
+  }
+
+  static reject(value) {
+    return new this((resolve, reject) => {
+      if (typeof resolve !== 'function' || typeof reject !== 'function') {
+        throw new TypeError('Not a function');
+      }
+      reject(value);
+    });
+  }
+
+  static all(promises) {
+    const isIterable = object => object !== null && typeof object[Symbol.iterator] === 'function';
+
+    return new OwnPromise((resolve, reject) => {
+      if (!isIterable(promises)) {
+        throw new TypeError('ERROR');
+      }
+
+      const values = new Array(promises.length);
+      let counter = 0;
+
+      const tryResolve = i => value => {
+        values[i] = value;
+        counter += 1;
+
+        if (counter === promises.length) {
+          resolve(values);
+        }
+      };
+
+      for (let i = 0; i < promises.length; i++) {
+        const promise = promises[i];
+        promise.then(tryResolve(i), reject);
+      }
+    });
   }
 
   static race(iterable) {
@@ -42,39 +183,6 @@ class OwnPromise {
         iterable[i].then(resolve, reject);
       }
     });
-  }
-
-  static all(promises) {
-    if ((!Array.isArray(promises)) || !(promises instanceof OwnPromise)) {
-      throw new TypeError('Promise.all arguments must be an array.');
-    } else {
-      const results = [];
-
-      return new OwnPromise((resolve, reject) => {
-        promises.forEach((promise, i) => {
-          if (!(promise instanceof OwnPromise)) {
-            throw new TypeError('variable must be a Promise instance.');
-          }
-          promise.then(res => {
-            results[i] = res;
-
-            if (results.length === promises.length) {
-              resolve(results);
-            }
-          }, err => {
-            reject(err);
-          });
-        });
-      });
-    }
-  }
-
-  static resolve(data) {
-    return new OwnPromise(resolve => resolve(data));
-  }
-
-  static reject(data) {
-    return new OwnPromise((resolve, reject) => reject(data));
   }
 }
 
